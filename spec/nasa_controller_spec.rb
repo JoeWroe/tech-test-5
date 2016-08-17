4c396c0470ede429a1d8e9eb23f30f7020642065
JoeWroe <joe_wroe@icloud.com>
2016-08-17 12:20:34 +0900

    Clean up nasa_controller_spec based on Rubocop

--- spec/nasa_controller_spec.rb
+++ spec/nasa_controller_spec.rb
@@ -1,34 +1,35 @@
 require 'nasa_controller'
 
 describe NasaController do
-  subject(:nasa_controller) { described_class.new(plateau: plateau) }
+  subject(:controller) { described_class.new(plateau: plateau) }
 
-  let(:robotic_rover) { double(:robotic_rover,
-                          land_rover: landed_rover,
-                          move_forward_on_x: display_move_pos,
-                          display_position: display_move_pos) }
-  let(:plateau)       { double(:plateau,
-                          one_rover_landed: landed_rover) }
+  let(:robotic_rover) do
+    double(:robotic_rover, land_rover: landed_rover,
+                           move_forward_on_x: display_move,
+                           display_position: display_move)
+  end
 
-  let(:landed_rover)     { [:landed_rover] }
-  let(:display_move_pos) { "Rovers position: 1 0 N" }
+  let(:plateau) { double(:plateau, rover_landed: landed_rover) }
 
-  let(:start_position)    { '0 0 N' }
-  let(:movement_commands) { 'M' }
+  let(:landed_rover) { [:landed_rover] }
+  let(:display_move) { 'Rovers position: 1 0 N' }
+
+  let(:start_pos)    { '0 0 N' }
+  let(:move_command) { 'M' }
 
   describe 'interacting with a rover' do
-    before { nasa_controller.link_to_rover(robotic_rover) }
+    before { controller.link_to_rover(robotic_rover) }
 
     it 'can create a link' do
-      expect(nasa_controller.current_rover).to eq robotic_rover
+      expect(controller.current_rover).to eq robotic_rover
     end
 
     it 'can land a rover' do
-      expect(nasa_controller.land_rover(start_position)).to eq plateau.one_rover_landed
+      expect(controller.land_rover(start_pos)).to eq plateau.rover_landed
     end
 
     it 'can read a command input' do
-      expect(nasa_controller.command_input(movement_commands)).to eq display_move_pos
+      expect(controller.command_input(move_command)).to eq display_move
     end
   end
 end
