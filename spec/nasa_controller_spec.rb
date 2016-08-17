91c7aa1b917269f114e72589641b27a5f436c1e5
JoeWroe <joe_wroe@icloud.com>
2016-08-17 12:07:05 +0900

    Refactor nasa_controller_spec
    ∆ Introduce a before loop to link to rover only once.
    
    ∆ Remove some lines that weren't being used anymore.

--- spec/nasa_controller_spec.rb
+++ spec/nasa_controller_spec.rb
@@ -5,39 +5,30 @@ describe NasaController do
 
   let(:robotic_rover) { double(:robotic_rover,
                           land_rover: landed_rover,
-                          move_forward_on_x: new_position,
-                          display_position: new_position) }
+                          move_forward_on_x: display_move_pos,
+                          display_position: display_move_pos) }
   let(:plateau)       { double(:plateau,
-                          no_rovers_landed: [],
                           one_rover_landed: landed_rover) }
 
-  let(:landed_rover)      { [:landed_rover] }
-  let(:new_position)      { "Rovers position: 1 0 N" }
+  let(:landed_rover)     { [:landed_rover] }
+  let(:display_move_pos) { "Rovers position: 1 0 N" }
 
   let(:start_position)    { '0 0 N' }
   let(:movement_commands) { 'M' }
 
+  describe 'interacting with a rover' do
+    before { nasa_controller.link_to_rover(robotic_rover) }
 
-  context 'on initialization' do
-    it 'has a plateau' do
-      expect(nasa_controller.plateau).to eq plateau
-    end
-  end
-
-  context 'interacting with a rover' do
     it 'can create a link' do
-      nasa_controller.link_to_rover(robotic_rover)
       expect(nasa_controller.current_rover).to eq robotic_rover
     end
 
     it 'can land a rover' do
-      nasa_controller.link_to_rover(robotic_rover)
       expect(nasa_controller.land_rover(start_position)).to eq plateau.one_rover_landed
     end
 
     it 'can read a command input' do
-      nasa_controller.link_to_rover(robotic_rover)
-      expect(nasa_controller.command_input(movement_commands)).to eq new_position
+      expect(nasa_controller.command_input(movement_commands)).to eq display_move_pos
     end
   end
 end
