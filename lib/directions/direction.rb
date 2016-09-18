5a15b432d331afbfb223d2b885f1f51762e32651
JoeWroe <joe_wroe@icloud.com>
2016-09-18 13:39:22 +0100

    Refactor direction to be a template class.
    ∆ Add all methods from Direction's sub-classes to Direction, these
    methods simply raise NotImplementedError. This should let any designer
    know that they need to implement a certain method in a new subclass if
    they write one.

--- lib/directions/direction.rb
+++ lib/directions/direction.rb
@@ -1,4 +1,19 @@
 # A template class for modules for each cardinal compass direction. This
 # will let the NavGrid know which direction the rover is facing.
 class Direction
+  def to_s
+    raise NotImplementedError
+  end
+
+  def right_turn
+    raise NotImplementedError
+  end
+
+  def left_turn
+    raise NotImplementedError
+  end
+
+  def move
+    raise NotImplementedError
+  end
 end
